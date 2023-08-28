afc6840a6440cfe01ad3355e6f74e145ef25eea9
Akhtar Alzrouni <138444303+AkhtarAlzarouni@users.noreply.github.com>
2023-08-28 21:15:07 +0400

    Initial commit

---
+++ bin/person.dart
@@ -0,0 +1,15 @@
+
+import 'Persons.dart';
+void main (){
+Persons a = Persons();
+a.getName();
+a.getAge();
+a.getEmail();
+a.setName("Akhtar ");
+a.setAge(23);
+a.setEmail("ek2000@gmail.com");
+ 
+ print(a.Name);
+ print(a.Age);
+ print(a.Email);
+}
